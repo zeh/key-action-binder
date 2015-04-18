4612f8bdf1b025c470ebd1313d814256c9d31714
zeh <zeh@zehfernando.com>
2015-04-18 18:32:43 -0400

    Basic gamepad events added, lots not implemented yet

--- src/core/KeyActionBinder.ts
+++ src/core/KeyActionBinder.ts
@@ -11,28 +11,294 @@ class KeyActionBinder {
 
 	// Properties
 	private _isRunning:boolean;
-	private _maintainPlayerPositions: boolean;
+	private _maintainPlayerPositions:boolean;
 
 	// Events
-	private _onActionActivated:zehfernando.signals.SimpleSignal<(action:string) => void>;
+	private _onActionActivated:zehfernando.signals.SimpleSignal<(action: string) => void>;
+	private _onActionDeactivated:zehfernando.signals.SimpleSignal<(action: string) => void>;
+	private _onActionValueChanged:zehfernando.signals.SimpleSignal<(action: string, value:number) => void>;
+	private _onDevicesChanged:zehfernando.signals.SimpleSignal<() => void>;
+	private _onRecentDeviceChanged:zehfernando.signals.SimpleSignal<(action: string) => void>; // TODO: should be GameInputDevice
 
+	private bindCache:any; // Should have been "{ [index: Function]: Function }", but that's not allowed
 
 
 	// ================================================================================================================
 	// CONSTRUCTOR ----------------------------------------------------------------------------------------------------
 
-	KeyActionBinder() {
-		
-	}
+	constructor() {
+		this.bindCache = {};
 
+		this._isRunning = false;
+		this._maintainPlayerPositions = false;
+		//this.bindings = new Array<BindingInfo>();
+		//actionsActivations = {};
+
+		this._onActionActivated = new zehfernando.signals.SimpleSignal<(action: string) => void>();
+		this._onActionDeactivated = new zehfernando.signals.SimpleSignal<(action: string) => void>();
+		this._onActionValueChanged = new zehfernando.signals.SimpleSignal<(action: string, value: number) => void>();
+		this._onDevicesChanged = new zehfernando.signals.SimpleSignal<() => void>();
+		this._onRecentDeviceChanged = new zehfernando.signals.SimpleSignal<(action: string) => void>();
+
+		//gameInputDevices = new Vector.<GameInputDevice>();
+		//gameInputDeviceIds = new Vector.<String>();
+		//gameInputDeviceDefinitions = new Vector.<AutoGamepadInfo>();
+
+		this.start();
+	}
 
 	// ================================================================================================================
 	// PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
 
+	/**
+	 * Starts listening for input events.
+	 *
+	 * <p>This happens by default when a KeyActionBinder object is instantiated; this method is only useful if
+	 * called after <code>stop()</code> has been used.</p>
+	 *
+	 * <p>Calling this method when a KeyActionBinder instance is already running has no effect.</p>
+	 *
+	 * @see #isRunning
+	 * @see #stop()
+	 */
+	public start(): void {
+		if (!this._isRunning) {
+			// Starts listening to keyboard events
+			window.addEventListener("keydown", this.getBoundFunction(this.onKeyDown));
+			window.addEventListener("keyup", this.getBoundFunction(this.onKeyUp));
+
+			// Starts listening to device change events
+			window.addEventListener("gamepadconnected", this.getBoundFunction(this.onGameInputDeviceAdded));
+			window.addEventListener("gamepaddisconnected", this.getBoundFunction(this.onGameInputDeviceRemoved));
+
+			this.refreshGameInputDeviceList();
+
+			this._isRunning = true;
+		}
+	}
+
+	/**
+	 * Stops listening for input events.
+	 *
+	 * <p>Action bindings are not lost when a KeyActionBinder instance is stopped; it merely starts ignoring
+	 * all input events, until <code>start()<code> is called again.</p>
+	 *
+	 * <p>This method should always be called when you don't need a KeyActionBinder instance anymore, otherwise
+	 * it'll be listening to events indefinitely.</p>
+	 *
+	 * <p>Calling this method when this a KeyActionBinder instance is already stopped has no effect.</p>
+	 *
+	 * @see #isRunning
+	 * @see #start()
+	 */
+	public stop(): void {
+		if (this._isRunning) {
+			// Stops listening to keyboard events
+			window.removeEventListener("keydown", this.getBoundFunction(this.onKeyDown));
+			window.removeEventListener("keyup", this.getBoundFunction(this.onKeyUp));
+
+			// Stops listening to device change events
+			window.removeEventListener("gamepadconnected", this.getBoundFunction(this.onGameInputDeviceAdded));
+			window.removeEventListener("gamepaddisconnected", this.getBoundFunction(this.onGameInputDeviceRemoved));
+
+			//gameInputDevices.length = 0;
+			//gameInputDeviceDefinitions.length = 0;
+			//removeGameInputDeviceEvents();
+
+			this._isRunning = false;
+		}
+	}
+
+
 	// ================================================================================================================
 	// ACCESSOR INTERFACE ---------------------------------------------------------------------------------------------
 
+	public get onActionActivated() {
+		return this._onActionActivated;
+	}
+
+	public get onActionDeactivated() {
+		return this._onActionDeactivated;
+	}
+
+	public get onActionValueChanged() {
+		return this._onActionValueChanged;
+	}
+
+	public get onDevicesChanged() {
+		return this._onDevicesChanged;
+	}
+
+	public get onRecentDeviceChanged() {
+		return this._onRecentDeviceChanged;
+	}
+
+	/**
+	 * Whether this KeyActionBinder instance is running, or not. This property is read-only.
+	 *
+	 * @see #start()
+	 * @see #stop()
+	 */
+	public get isRunning(): boolean {
+		return this._isRunning;
+	}
+
+
+	// ================================================================================================================
+	// EVENT INTERFACE ------------------------------------------------------------------------------------------------
+
+	private onKeyDown(e: KeyboardEvent): void {
+		console.error("NOT IMPLEMENTED: onKeyDown");
+	}
+
+	private onKeyUp(e: KeyboardEvent): void {
+		console.error("NOT IMPLEMENTED: onKeyUp");
+	}
+
+	private onGameInputDeviceAdded(e: GamepadEvent): void {
+		console.error("NOT IMPLEMENTED: onGameInputDeviceAdded");
+	}
+
+	private onGameInputDeviceRemoved(e: GamepadEvent): void {
+		console.error("NOT IMPLEMENTED: onGameInputDeviceRemoved");
+	}
+
 	// ================================================================================================================
 	// PRIVATE INTERFACE ----------------------------------------------------------------------------------------------
+
+	private refreshGameInputDeviceList(): void {
+		// The list of game devices has changed
+
+		console.error("NOT IMPLEMENTED: refreshGameInputDeviceList");
+
+		/*
+
+		// Check if there was any actual change to the list
+		// This is necessary because some versions of Flash keep firing the changed event every second (Windows 7 + Firefox using Flash Player 12.0.0.44)
+		var i: int;
+		var hasChanged: Boolean = false;
+
+		if (GameInput.numDevices != gameInputDevices.length) {
+			hasChanged = true;
+		} else {
+			for (i = 0; i < GameInput.numDevices; i++) {
+				if (gameInputDevices[i] != GameInput.getDeviceAt(i)) {
+					hasChanged = true;
+					break;
+				}
+			}
+		}
+
+		if (hasChanged) {
+			// List has actually changed
+			removeGameInputDeviceEvents();
+			addGameInputDeviceEvents();
+
+			if (_maintainPlayerPositions && gameInputDeviceIds != null) {
+				// Will try to maintain player positions:
+				// * A removed device will continue to exist in the list (as a null device), unless it's the latest device
+				// * An added device will try to be added to its previously existing position, if one can be found
+				// * If a previously existing position cannot be found, the device takes the first available position
+
+				var gamepadPosition: int;
+
+				// Creates a list of all the new ids
+				var newGamepadIds: Vector.<String> = new Vector.<String>();
+				var newGamepads: Vector.<GameInputDevice> = new Vector.<GameInputDevice>();
+				for (i = 0; i < GameInput.numDevices; i++) {
+					if (GameInput.getDeviceAt(i) != null) {
+						newGamepadIds.push(GameInput.getDeviceAt(i).id);
+						newGamepads.push(GameInput.getDeviceAt(i));
+					}
+				}
+
+				// Create a list of available slots for insertion
+				var availableSlots: Vector.<int> = new Vector.<int>();
+
+				// First, check for removed items
+				// Goes backward, so it can remove items from the list
+				var isEndPure: Boolean = true;
+				i = gameInputDeviceIds.length - 1;
+				while (i >= 0) {
+					gamepadPosition = newGamepadIds.indexOf(gameInputDeviceIds[i]);
+					if (gamepadPosition < 0) {
+						// This device id doesn't exist in the new list, therefore it's removed
+						if (isEndPure) {
+							// But since it's in the end of the list, actually remove it
+							gameInputDeviceIds.splice(i, 1);
+						} else {
+							// It's in the middle of the list, so just mark that spot as available
+							availableSlots.push(i);
+						}
+					} else {
+						// This device id exists in the list, so ignore and assume it's not in the end anymore
+						isEndPure = false;
+					}
+					i--;
+				}
+
+				// Now, add new items that are not in the list
+				for (i = 0; i < newGamepadIds.length; i++) {
+					gamepadPosition = gameInputDeviceIds.indexOf(newGamepadIds[i]);
+					if (gamepadPosition < 0) {
+						// This gamepad is not in the list, so add it
+						if (availableSlots.length > 0) {
+							// Add it in the first available slot
+							gameInputDeviceIds.push(newGamepadIds[availableSlots[0]]);
+							availableSlots.splice(0, 1);
+						} else {
+							// No more slots availabloe, add it at the end
+							gameInputDeviceIds.push(newGamepadIds[i]);
+						}
+					}
+				}
+
+				// Now that gameInputDeviceIds is correct, just create the list of references
+				gameInputDevices.length = gameInputDeviceIds.length;
+				gameInputDeviceDefinitions.length = gameInputDeviceIds.length;
+				for (i = 0; i < gameInputDeviceIds.length; i++) {
+					gamepadPosition = newGamepadIds.indexOf(gameInputDeviceIds[i]);
+					if (gamepadPosition < 0) {
+						// A spot for a gamepad that was just removed
+						gameInputDevices[i] = null;
+						gameInputDeviceDefinitions[i] = null;
+					} else {
+						// A normal game input device
+						gameInputDevices[i] = newGamepads[gamepadPosition];
+						gameInputDeviceDefinitions[i] = findGamepadInfo(newGamepads[gamepadPosition]);
+					}
+				}
+			} else {
+				// Full refresh: create a new list of devices
+				gameInputDevices.length = GameInput.numDevices;
+				gameInputDeviceIds.length = GameInput.numDevices;
+				gameInputDeviceDefinitions.length = GameInput.numDevices;
+				for (i = 0; i < GameInput.numDevices; i++) {
+					gameInputDevices[i] = GameInput.getDeviceAt(i);
+					if (gameInputDevices[i] != null) {
+						gameInputDeviceIds[i] = gameInputDevices[i].id;
+						gameInputDeviceDefinitions[i] = findGamepadInfo(gameInputDevices[i]);
+					} else {
+						gameInputDeviceIds[i] = null;
+						gameInputDeviceDefinitions[i] = null;
+					}
+				}
+			}
+
+			// Dispatch the signal
+			_onDevicesChanged.dispatch();
+		}
+		*/
+	}
+
+	/**
+	 * Utility function: creates a function bound to "this". This is needed because the same reference needs to be used when removing listeners.
+	 */
+	private getBoundFunction(func:any): EventListener {
+		if (!this.bindCache.hasOwnProperty(func)) {
+			this.bindCache[func] = func.bind(this);
+		}
+		return this.bindCache[func];
+	}
 	
 }
