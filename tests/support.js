37d56e08d61eed417915ef1fd53d9939aa20b3cf
zeh <zeh@zehfernando.com>
2015-04-27 14:56:29 -0400

    Safer support test file

--- tests/support.js
+++ tests/support.js
@@ -2,5 +2,5 @@ var test = require('tape');
 
 test('Gamepad support', function (t) {
 	t.plan(1);
-    t.equal(typeof navigator.getGamepads, "function");
+    t.equal(typeof(navigator) != "undefined" && typeof navigator.getGamepads == "function", true);
 });
